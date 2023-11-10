62b4ed864c5d13d85c030bdad322dc1724245e83
Carlos Ochoa <carlosochoala03@gmail.com>
2023-11-10 20:30:06 +0100

    funciones

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-let tareas =  [];
+var tareas =  [];
+
+function reverseCadena(cadena){
+    let caracteres = cadena.split("-");
+    let caracteresInvertidos = caracteres.reverse();
+    let cadenaInvertida = caracteresInvertidos.join("-");
+    return cadenaInvertida;
+}
+
+function horasMinutos(valor){
+    //horas
+    let horas = Math.floor(valor/60);
+
+    //minutos retantes
+    let minutosRestantes = valor%60;
+    let array = [horas,minutosRestantes];
+    return array;
+
+}
+
+function cadenaEstimado(array){
+    return array[0] + "h " + array[1] + "min";
+}
+
+
+
+
 
 
 document.getElementById("formulario").addEventListener('submit',function(event){
@@ -6,21 +32,27 @@ document.getElementById("formulario").addEventListener('submit',function(event){
     let nombre = document.getElementById("nombre").value;
     let estimado = document.getElementById("estimado").value;
     let finalizacion = document.getElementById("finalizacion").value;
+    let finalizacionInvert = reverseCadena(finalizacion);
     let alta = document.getElementById("alta").value;
-
+    let altaInvert = reverseCadena(alta);
     let selectElemento = document.getElementById("select");
     let option = selectElemento.options[selectElemento.selectedIndex].text;
 
     const nuevaTarea = {
         nombre: nombre,
         estimado: estimado,
-        finalizacion: finalizacion,
-        alta: alta,
+        finalizacion: finalizacionInvert,
+        alta: altaInvert,
         option: option
     };
     tareas.push(nuevaTarea);
 
+    mostrarTarea(nuevaTarea);
+    
+});
 
+function mostrarTarea(tarea){
+    const copiaTarea = {...tarea};
 
     const contTabla = document.createElement("div");
     contTabla.className = "contTabla";
@@ -28,12 +60,14 @@ document.getElementById("formulario").addEventListener('submit',function(event){
 
     const fila = tabla.insertRow();
     fila.classList.add("nueva-fila");
-    ultimo = Object.values(tareas[tareas.length-1]);
 
-    ultimo.forEach(function(valor){
+    let array = horasMinutos(copiaTarea.estimado);
+    let mostrarEstimado = cadenaEstimado(array);
+    copiaTarea.estimado = mostrarEstimado;
+    for(let valor in tarea){
         const celda = fila.insertCell();
-        celda.textContent = valor;
-    });
+        celda.textContent = copiaTarea[valor];
+    }
     const celdaMod = fila.insertCell()
     const modificar = document.createElement("button");
     modificar.textContent = "Modificar";
@@ -51,7 +85,23 @@ document.getElementById("formulario").addEventListener('submit',function(event){
         tabla.deleteRow(rowIndex);
         tareas.splice(rowIndex - 1, 1);
     });
+}
+
+function mostrarOrdenado(){
+    const tabla = document.querySelector("table");
+    tabla.innerHTML = '<thead class="cabecera"><tr><th>Nombre</th><th>Tiempo estimado</th><th>Fecha máx. finalización</th><th>Fecha de alta</th><th>Estado</th><th>Modificar</th><th>Borrar</th></tr></thead>';
+    for(let i=0;i<tareas.length;i++){
+        mostrarTarea(tareas[i]);
+    }
+}
+
+document.getElementById("estimadoDESC").onclick = function(){
+    tareas.sort(((a, b) => parseFloat(a.estimado) - parseFloat(b.estimado)));
+    mostrarOrdenado();
+    
+};
 
 
-});
+
+
 
